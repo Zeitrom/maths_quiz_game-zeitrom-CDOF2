bf5729490f35e8cd9e6fc18bb7e1c3e9fe3b1955
Zeitrom <hugo.verley@gmail.com>
2024-01-30 09:00:09 +0100

    setup informations

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{maths_quiz_game-zeitrom-CDOF2}',
+ version='1.0',
+ author='{Zeitrom}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
